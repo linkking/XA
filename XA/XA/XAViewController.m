b116ad209b55bb2d39b40c22e88c231dfc534e6f
zhulei <zhulei6@xin.com>
2018-05-24 17:08:25 +0800

    修改页面颜色

--- XA/XA/XAViewController.m
+++ XA/XA/XAViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"XA";
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
 - (void)didReceiveMemoryWarning {
